06823cf2ac1c1ae1d072155a853f3a9b9b893e47
bibixx <bartosz@legiec.io>
2021-03-16 20:21:40 +0100

    Fix encoding

--- TPO1/src/zad1/Futil.java
+++ TPO1/src/zad1/Futil.java
@@ -1,6 +1,9 @@
 package zad1;
 import java.io.*;
+import java.nio.ByteBuffer;
+import java.nio.CharBuffer;
 import java.nio.channels.FileChannel;
+import java.nio.charset.Charset;
 import java.nio.file.*;
 import java.nio.file.attribute.BasicFileAttributes;
 
@@ -8,23 +11,37 @@ public class Futil {
 	public static void processDir(String dirName, String resultFileName) {
         Path startPath = Paths.get(dirName);
         try {
-        	FileChannel dstChannel = new FileOutputStream(resultFileName).getChannel();
+        	FileOutputStream fos = new FileOutputStream(resultFileName);
+        	FileChannel dstChannel = fos.getChannel();
+        	Charset outputCharset = Charset.forName("UTF-8");
+
 			Files.walkFileTree(startPath, new SimpleFileVisitor<Path>() {
-				private int position = 0;
-				
 			    public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException
 			    {
-			        FileChannel srcChannel = new FileInputStream(file.toFile()).getChannel();
+			    	FileInputStream fis = new FileInputStream(file.toFile());
+			        FileChannel srcChannel = fis.getChannel();
+			        
+			        ByteBuffer buffer = ByteBuffer.allocateDirect(256);
 
-			        dstChannel.transferFrom(srcChannel, this.position, srcChannel.size());
-			        this.position += srcChannel.size();
+			        Charset inputCharset = Charset.forName("windows-1250");
+			        
+			        while (srcChannel.read(buffer) != -1) {
+			            buffer.rewind();
+			            CharBuffer charBuffer = inputCharset.decode(buffer);
+			            dstChannel.write(outputCharset.encode(charBuffer));
+
+			            buffer.clear();
+			        }
+			        
 			        srcChannel.close();
+			        fis.close();
 
 			        return FileVisitResult.CONTINUE;
 			    }
 			});
 			
 			dstChannel.close();
+			fos.close();
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
